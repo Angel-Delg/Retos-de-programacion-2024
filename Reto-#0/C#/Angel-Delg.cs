3e78747481730f51a6d9e3788285103dc1506a80
Angel Delgado <lariosangel062@gmail.com>
2024-01-01 02:48:05 -0600

    Reto#0 C, C#, Javascript, Python

---
+++ Reto-#0/C#/Angel-Delg.cs
@@ -0,0 +1,26 @@
+using System;
+
+class Program {
+   static void Main(){
+      // URL = https://dotnet.microsoft.com/es-es/
+      /*
+         Commentario de varias Lineas.
+         URL = https://dotnet.microsoft.com/es-es/
+      */
+
+      int Variable = 2024;
+      const decimal TasaCambio = 36.6598;
+
+      // tipos de datos primitivos
+      int Entero = 2024;
+      decimal Decimal = 20.24;
+      float Flotante = 20.24f;
+      string CadenaTexto = "2024";
+      bool isNewYear = true;
+
+      /* etc...*/
+      
+      Console.WriteLine("Hola c#");
+      return;
+   }
+}
